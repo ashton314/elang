68891d0ff903810f3515bd308968986e0b0674e7
Ashton Wiersdorf <ashton.wiersdorf@pobox.com>
2022-03-23 11:28:05 -0600

    Expand Ast enum definition to list the primitives I want to support
    This is what I will be using for the code gen phase.

--- src/ast.rs
+++ src/ast.rs
@@ -1,9 +1,16 @@
+/// Ast enum represents the processed AST we get from the Racket side of the compiler.
 #[derive(Debug)]
 pub enum Ast<'a> {
     // Integers are the only numbers supported so far
     Int(i64),
-    // Variable references (includes references to built-in operators like `+`)
+    // Variables
     Var(&'a str),
-    // Lists, of which function calls are a special case
-    Lst(Vec<&'a Ast<'a>>)
+    // Top-level definitions
+    Defn(&'a str, Box<Ast<'a>>),
+    // Function calls
+    Funcall(&'a str, Vec<Ast<'a>>),
+    // Lambdas: argument list and list of exprs as body
+    Lambda(Vec<&'a str>, Vec<Ast<'a>>),
+    // Let blocks: list of tuples var → binding; list of exprs as body
+    Let(Vec<(&'a str, Ast<'a>)>, Vec<Ast<'a>>)
 }
