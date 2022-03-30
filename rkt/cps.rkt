342260dde922b0d5f84994b6496585332eb6238e
Ashton Wiersdorf <ashton.wiersdorf@pobox.com>
2022-03-30 11:11:22 -0600

    Tiny bit of work on the cps converter

--- rkt/cps.rkt
+++ rkt/cps.rkt
@@ -4,10 +4,18 @@
 
 ;;; CPS conversion module
 
-(define (->cps expr κ)
+(define (->cps expr [κ 'κ₀])
   (match expr
     [(? symbol? e) `(,κ ,e)]
     [(? number? e) `(,κ ,e)]
+    ;; [`(app ,f)
+    ;;  ]
+    ;; [`(app ,f ,as ...)
+    ;;  ]
+    [`(,(or 'lambda 'λ) (,params ...) ,body)
+     (let ([k1 (gensym 'κ)])
+       `(kapp ,κ (λ (,@params ,k1)
+              ,(->cps body k1))))]
     [`(if ,tst ,tc ,fc)
      (let ([k1 (gensym 'κ)]
            [ctc (->cps tc κ)]
