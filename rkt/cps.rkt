c1687a6d3d50b08194b7df4d7d786627be895af4
Ashton Wiersdorf <ashton.wiersdorf@pobox.com>
2022-04-22 16:22:40 -0600

    Add CPS for appliction forms

--- rkt/cps.rkt
+++ rkt/cps.rkt
@@ -1,21 +1,43 @@
 #lang racket
 
-(provide (all-defined-out))
+(provide ->cps)
 
 ;;; CPS conversion module
 
+(define (simple-exp? e)
+  (or (number? e) (symbol? e)))
+
 (define (->cps expr [κ 'κ₀])
   (match expr
-    [(? symbol? e) `(,κ ,e)]
-    [(? number? e) `(,κ ,e)]
-    ;; [`(app ,f)
-    ;;  ]
-    ;; [`(app ,f ,as ...)
-    ;;  ]
+    [(? simple-exp? e) `(,κ ,e)]
+
+    [`(app ,(? simple-exp? f))
+     `(fapp ,f ,κ)]
+
+    [`(app ,(? simple-exp? f) ,args ...)
+     ;; cps args and then call
+
+     (define (cps-args cpsd todo)
+       (if (null? todo)
+           `(fapp ,f ,@cpsd ,κ)
+           (if (simple-exp? (car todo))
+               (cps-args (cons (car todo) cpsd) (cdr todo))
+               ;; convert head of todo
+               (->cps (car todo)
+                      (let ([k-fresh (gensym 'κ)])
+                        `(λ (,k-fresh)
+                           ,(->cps `(app ,f ,@cpsd ,k-fresh ,@(cdr todo)))))))))
+     (cps-args '() args)]
+
+    [`(app ,f ,as ...)
+     (->cps f (let ([k-fresh (gensym 'κ)])
+                `(λ (,k-fresh) ,(->cps `(app ,k-fresh ,@as)))))]
+
     [`(,(or 'lambda 'λ) (,params ...) ,body)
      (let ([k1 (gensym 'κ)])
        `(kapp ,κ (λ (,@params ,k1)
               ,(->cps body k1))))]
+
     [`(if ,tst ,tc ,fc)
      (let ([k1 (gensym 'κ)]
            [ctc (->cps tc κ)]
